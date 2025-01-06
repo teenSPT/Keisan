831494f8d333031bd9c403afd7fea6f7972583bd
teenSPT <neosis0101@gmail.com>
2025-01-07 00:06:54 +0900

    script.js

--- script.js
+++ script.js
@@ -1,41 +1,101 @@
-    const spreadData = {
-      'BTC': { 'XM': 300, 'TITAN': 100 },
-      'ETH': { 'XM': 150, 'TITAN': 200 },
-      'SOL': { 'XM': 250, 'TITAN': 180 }
-    };
+let selectedTicker = 'Ticker';
+let selectedTime = 'Time';
+let selectedBlock = 'Block';
+let selectedB2tm = 'Time';
+let selectedB2blk = 'Block';
+let selectedTfc = 'Time';
+let selectedDecisiveTime = 'Time';
+let selectedDecisiveBlock = 'Block';
+let selectedC1tm = 'Time';
+let selectedC1nc = 'c';
+let selectedC2tm = 'Time';
+let selectedC2nc = 'c';
+let selectedC3tm = 'Time';
+let selectedC3nc = 'c';
+let selectedBroker = 'Broker';
 
-    let selections = {
-      ticker: '',
-      time: '',
-      block: '',
-      broker: ''
-    };
+function updateTicker(value) {
+    selectedTicker = value;
+}
 
-    function updateSelection(type) {
-      selections[type] = document.getElementById(type).value;
-    }
+function updateTime(value) {
+    selectedTime = value;
+}
 
-    function executeSelection() {
-      const { ticker, time, block, broker } = selections;
-      const spread = spreadData[ticker] ? spreadData[ticker][broker] : "N/A";
+function updateBlock(value) {
+    selectedBlock = value;
+}
 
-      // 結果を表示
-      const resultDiv = document.getElementById('result');
-      const spreadDiv = document.getElementById('spread');
+function updateB2tm(value) {
+    selectedB2tm = value;
+}
 
-      if (ticker && time && block && broker) {
-          resultDiv.innerHTML = `
-              <strong>選択結果:</strong><br>
-               ${ticker} ${time} ${block} ${broker}
-          `;
-          spreadDiv.innerHTML = `<strong>Spread:</strong> ${spread}`;
-      } else {
-          resultDiv.innerHTML = "<strong>すべての項目を選択してください。</strong>";
-          spreadDiv.innerHTML = ""; // スプレッドの表示をクリア
-      }
-    }
+function updateB2blk(value) {
+    selectedB2blk = value;
+}
 
-    function copyToClipboard() {
-      const resultDiv = document.getElementById('result');
-      const spreadDiv = document.getElementById('spread');
-      const textToCopy = `${resultDiv.innerText}\n${spreadDiv.innerText}`;
+function updateTfc(value) {
+    selectedTfc = value;
+}
+
+function updateDecisiveTime(value) {
+    selectedDecisiveTime = value;
+}
+
+function updateDecisiveBlock(value) {
+    selectedDecisiveBlock = value;
+}
+
+function updateC1tm(value) {
+    selectedC1tm = value;
+}
+
+function updateC1nc(value) {
+    selectedC1nc = value;
+}
+
+function updateC2tm(value) {
+    selectedC2tm = value;
+}
+
+function updateC2nc(value) {
+    selectedC2nc = value;
+}
+
+function updateC3tm(value) {
+    selectedC3tm = value;
+}
+
+function updateC3nc(value) {
+    selectedC3nc = value;
+}
+
+function updateBroker(value) {
+    selectedBroker = value;
+}
+
+function executeSelection() {
+    const resultDiv = document.getElementById('result');
+    const resultInfo = `
+        ${selectedTicker}: ${selectedTime} ${selectedBlock}   
+        ${selectedB2tm} ${selectedB2blk} / TFC: ${selectedTfc} / 
+        決定打: ${selectedDecisiveTime} ${selectedDecisiveBlock} / 
+        ...${selectedC1tm} ${selectedC1nc} / 
+        ${selectedC2tm} ${selectedC2nc} / 
+        ${selectedC3tm} ${selectedC3nc}...
+        ${selectedBroker}
+    `;
+    resultDiv.innerText = resultInfo;
+}
+
+function copyInfo() {
+    const resultDiv = document.getElementById('result');
+    navigator.clipboard.writeText(resultDiv.innerText).then(() => {
+        alert('情報がコピーされました！');
+    });
+}
+
+function copySpread() {
+    // Spreadの計算ロジックをここに追加することができます
+    alert('Spreadのコピー機能は未実装です。');
+}
